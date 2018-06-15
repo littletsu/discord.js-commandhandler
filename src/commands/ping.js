f4f8f476da202b056c74265ce651d955f9540a23
Etzyy <38116582+Etzyy@users.noreply.github.com>
2018-06-15 10:36:05 -0500

    Create ping.js

---
+++ src/commands/ping.js
@@ -0,0 +1,27 @@
+// This is a template.
+
+module.exports.run = (client, message, args, utils) => {
+
+    // Command starts here. You can use the variables: "client", "message", "args" and "utils" (Which I think is all you need.)
+
+    // Example of code
+    message.channel.send('pong, wow i am a smart bot.')
+
+
+}
+
+// Configuration
+
+module.exports.config = {
+    args: false, /* The command requires arguments? Could be false or true. */
+    restricted: false, /* Can only owner use the command? Could be false or true. */
+    category: 'Utils' /* You can make a category help command with this. */
+}
+
+// Aliases, name, description and usage
+
+module.exports.help = {
+    name: 'ping',
+    description: 'pong wow!',
+    usage: 'PREFIXping'
+}
